53a059a5588e4e1b4be9c7c9e4c00de5af9aeace
timmit147 <timmeeuwsen@hotmail.nl>
2023-07-23 13:37:57 +0000

    Update file with fetched JSON data

--- js/json.js
+++ js/json.js
@@ -810,6 +810,48 @@
                       "format": {}
                     }
                   ]
+                },
+                {
+                  "userEnteredValue": {
+                    "formulaValue": "=IMAGE(\"https://drive.google.com/uc?id=1JSd_laFUxyIFIJoQuZgMMq2zpN1KToww&export=download\")"
+                  },
+                  "effectiveValue": {},
+                  "effectiveFormat": {
+                    "backgroundColor": {
+                      "red": 1,
+                      "green": 1,
+                      "blue": 1
+                    },
+                    "padding": {
+                      "top": 2,
+                      "right": 3,
+                      "bottom": 2,
+                      "left": 3
+                    },
+                    "horizontalAlignment": "LEFT",
+                    "verticalAlignment": "BOTTOM",
+                    "wrapStrategy": "OVERFLOW_CELL",
+                    "textFormat": {
+                      "foregroundColor": {},
+                      "fontFamily": "Arial",
+                      "fontSize": 10,
+                      "bold": false,
+                      "italic": false,
+                      "strikethrough": false,
+                      "underline": false,
+                      "foregroundColorStyle": {
+                        "rgbColor": {}
+                      }
+                    },
+                    "hyperlinkDisplayType": "PLAIN_TEXT",
+                    "backgroundColorStyle": {
+                      "rgbColor": {
+                        "red": 1,
+                        "green": 1,
+                        "blue": 1
+                      }
+                    }
+                  }
                 }
               ]
             },
